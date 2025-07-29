5f7f7223716eaf1f991fda3f9512c59d525f5ec3
ishan <prajapatiishan.ip6@gmail.com>
2025-07-29 15:23:12 +0530

    comit code 001

---
+++ src/main/java/bot/BotLogin.java
@@ -0,0 +1,44 @@
+package bot;
+
+import bot.base.BotBase;
+import bot.botChat.BotList;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.lang.reflect.InvocationTargetException;
+import java.time.Duration;
+
+public class BotLogin extends BotBase {
+    public BotLogin(ChromeDriver chromeDriver) {
+        super(chromeDriver);
+        PageFactory.initElements(chromeDriver, this);
+    }
+
+
+    @FindBy(id = "mui-1")
+    WebElement mobileNumberInput;
+    @FindBy(xpath = "//button/p[text()='Send OTP']")
+    WebElement sendOTPButton;
+//button//p/p[text()='Continue']
+
+    public BotList loginWithMobileNumber(String mobileNumber) throws InvocationTargetException, InstantiationException, IllegalAccessException, NoSuchMethodException {
+        try {
+            // Enter the mobile number
+            mobileNumberInput.clear(); // Clear the input field in case it contains default text.
+            mobileNumberInput.sendKeys(mobileNumber);
+
+            // Click the "Send OTP" button
+            sendOTPButton.click();
+
+            System.out.println("Login successful: OTP sent to " + mobileNumber);
+        } catch (Exception e) {
+            System.err.println("Failed to login using mobile number: " + e.getMessage());
+        }
+        return createBotPageForChrome(BotList.class);
+    }
+}
