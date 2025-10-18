e300f212cc2961c9e1f2583958feddf4ee81234b
CG-AutomationGit <testingcggujarat@gmail.com>
2025-10-18 21:08:38 +0530

    Add import for java.util package

--- src/main/java/bot/botChat/Chat.java
+++ src/main/java/bot/botChat/Chat.java
@@ -17,6 +17,7 @@ import org.openqa.selenium.support.ui.Select;
 import org.testng.Assert;
 import org.testng.asserts.SoftAssert;
 import utility.LangMapper;
+import java.util.*;
 
 import java.io.File;
 import java.io.FileInputStream;
